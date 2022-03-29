c68dbf82560747f59554d74f578229ed591f38bf
John-droid-dotcom <83913756+John-droid-dotcom@users.noreply.github.com>
2022-03-29 22:36:17 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{html,js}",
+        "./components/**/*.{html,js}",
+        "./utilities/**/*.{html,js}",
+        "./dist/index.html",
+        // "./pages/**/*.{html,js}",
+        // "./components/**/*.{html,js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
